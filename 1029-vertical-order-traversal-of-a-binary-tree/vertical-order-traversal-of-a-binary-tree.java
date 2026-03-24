e31900b0486d825be3643b615edb053d7b585b4a
sandiyasamuthiravel <sandiyasam556@gmail.com>
2026-03-24 21:51:50 +0530

    Time: 4 ms (61.56%) | Memory: 44.1 MB (74.86%) - LeetSync

---
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Pair {
+    TreeNode node;
+    int row, col;
+    
+    Pair(TreeNode node, int row, int col) {
+        this.node = node;
+        this.row = row;
+        this.col = col;
+    }
+}
+class Solution {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 0, 0)); // node, row, col
+        while (!queue.isEmpty()) {
+            Pair t = queue.poll();
+            TreeNode node = t.node;
+            int row = t.row;
+            int col = t.col;
+            // replace putIfAbsent
+            if (!map.containsKey(col)) {
+                map.put(col, new TreeMap<>());
+            }
+            if (!map.get(col).containsKey(row)) {
+                map.get(col).put(row, new PriorityQueue<>());
+            }
+            map.get(col).get(row).offer(node.val);
+            if (node.left != null) {
+                queue.offer(new Pair(node.left, row + 1, col - 1));
+            }
+            if (node.right != null) {
+                queue.offer(new Pair(node.right, row + 1, col + 1));
+            }
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        for (TreeMap<Integer, PriorityQueue<Integer>> ys : map.values()) {
+            List<Integer> list = new ArrayList<>();
+            for (PriorityQueue<Integer> nodes : ys.values()) {
+                while (!nodes.isEmpty()) {
+                    list.add(nodes.poll());
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
+
+
